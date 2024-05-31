f2375f35778182de7db104d625e4800cccad3db6
Ander Endara <157659704+ander24508e@users.noreply.github.com>
2024-05-31 15:35:39 -0500

    Update bussinesRoutes.js
    cuarto commit

--- src/routes/bussinesRoutes.js
+++ src/routes/bussinesRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { createBusiness, getAllBusinesses } = require('../controllers/businessController');
+const { createBusiness, getAllBusinesses } = require('../controllers/bussinesController');
 const router = express.Router();
 
 router.post('/create_business', createBusiness);
 router.get('/get_all_businesses', getAllBusinesses);
 
 module.exports = router;
+
